220f5d3e0330bb759e0b488a00318de439d2f782
Patryk Pieniążek <patryk.pieniazek2001@gmail.com>
2026-07-18 08:56:22 +0200

    Updated AI Agent course: end of chapter 1

--- README.md
+++
@@ -1,3 +0,0 @@
-# Build an AI Agent in Python
-
-[Boot.dev](https://www.boot.dev/) guided project.
--- main.py
+++ main.py
@@ -2,29 +2,44 @@ import argparse
 import os
 
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from openai import OpenAI
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if not api_key:
-    raise RuntimeError("GEMINI_API_KEY environment variable not set")
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Api key not found in the environment.")
 
-client = genai.Client(api_key=api_key)
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+    generate_content(client, messages, args.verbose)
 
-if not response.usage_metadata:
-    raise RuntimeError("Usage metadata not available in response")
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-print(f"Response:\n{response.text}")
+def generate_content(client: OpenAI, messages: list, verbose: bool) -> None:
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+    if response.usage is None:
+        raise RuntimeError("Failed request")
+    if verbose:
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response:\n{response.choices[0].message.content}")
+
+
+if __name__ == "__main__":
+    main()
